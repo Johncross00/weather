e841710ec09714c8ad1bb6ba32ebb238c63750ca
johncross00 <johncross.ktm@gmail.com>
2024-04-26 00:13:11 +0000

    lotties

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -43,6 +43,16 @@ class _WeatherPageState extends State<WeatherPage> {
   }
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(_weather?.cityName ?? "Loading city.."),
+            Text('${_weather?.temperature.round()}°C'),
+          ],
+        ),
+      ),
+    );
   }
 }
